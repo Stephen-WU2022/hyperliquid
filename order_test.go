a7e4fdf621070a1dfa8c9120fa49f0c8bea8e819
Stephen <tama82045@gmail.com>
2025-07-23 20:15:27 +0800

    can't identify  User or API Wallet 0x000... does not exist error

--- order_test.go
+++ order_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Test_Order(t *testing.T) {
-	client := NewClient("0xb827b5f16be8b75b5eb29202bce54eaeada6cebd", "0x9c0d9ff46743eece3fd414176c8316635c0bbdf93258a760aca8e38c1df9decc", nil)
+	client := NewClient("0xb827b5f16be8b75b5eb29202bce54eaeada6cebd", "", nil)
 
 	openOrderInfo, err := client.PlaceOrder("limit", "ETH", "buy", 0.003, 3000)
 	if err != nil {
